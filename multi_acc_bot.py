96828b935d79b1bd88ec07d45d1a0ee403449e5f
Seakleang <nhakseakleang@gmail.com>
2022-10-04 14:48:49 +0700

    add multi and timer bet

--- multi_acc_bot.py
+++ multi_acc_bot.py
@@ -46,11 +46,11 @@ logger = logging.getLogger(__name__)
 tempAccount = {}
 loggedAccounts = []
 
-ADD_ACC, ENTER_PHONE, ENTER_PASS, ENTER_SERVER, LOGGED_IN, RE_LOGIN, COMPETITION, ORDER, RECORD = range(9)
+ADD_ACC, ADD_MULTI, ENTER_PHONE, ENTER_PASS, ENTER_SERVER, LOGGED_IN, RE_LOGIN, COMPETITION, ORDER, RECORD, SET_BET_TIMER, UN_SET_BET_TIMER = range(12)
 Q_INFO, Q_ORDER = range(2)
 
 start_keyboard = [
-    ["Add New Account"],
+    ["Add New Account", "Add Multi"],
     # ["Number of siblings", "Something else..."],
     # ["Done"],
 ]
@@ -59,22 +59,28 @@ start_markup = ReplyKeyboardMarkup(
 
 logged_keyboard = [
     ["Balance", "Competition"],
-    ["Bet record", "Re-Login"],
-    ["Add New Account"]
+    ["Bet record", "Login"],
+    ["/SetBet 3600", "/UnSetBet"],
+    ["Add New Account", "Add Multi"],
+    ["Log out", "Done"]
 ]
 logged_markup = ReplyKeyboardMarkup(
     logged_keyboard, input_field_placeholder="Choose the option...", resize_keyboard=True)
 
+timer = 0.0
 
 def facts_to_str(user_data: Dict[str, str]) -> str:
     """Helper function for formatting the gathered user info."""
     facts = [f"{key} - {value}" for key, value in user_data.items()]
-    return "\n".join(facts).join(["\n", "\n"])
+    return "\n".join(facts).join(["\n", "\n"]).replace("'", '"')
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    global loggedAccounts
+
     """Start the conversation, display any stored data and ask user for input."""
     if "account" in context.user_data and len(context.user_data["account"]) > 0:
+        loggedAccounts = context.user_data["account"]
         reply_text = "You're already added account.\nPlease tell what you want me to do."
 
         await update.message.reply_text(reply_text, reply_markup=logged_markup)
@@ -85,6 +91,64 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         await update.message.reply_text(reply_text, reply_markup=start_markup)
         return ADD_ACC
 
+async def add_multi(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    reply_text = "Please enter your accounts json:"
+    await update.message.reply_text(reply_text, reply_markup=ReplyKeyboardRemove())
+
+    return ADD_MULTI
+
+async def json_to_accounts(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    print(f"Add Multi: {update.message.text}")
+    global loggedAccounts
+    accounts = json.loads(update.message.text)
+    oldAccount = []
+
+    for account in accounts:
+        phone = account["phone"]
+        password = account["pass"]
+        server = account["server"]
+        url = (f"{server}/api/login")
+        payload = {
+            'mobile': phone,
+            'password': password
+        }
+
+        try:
+            r = requests.post(url, json=payload)
+            r.raise_for_status()
+
+            if context.user_data.get("account"):
+                oldAccount = context.user_data["account"]
+
+            oldAccount.append({"phone": phone, "pass": password, "server": server})
+            context.user_data["account"] = oldAccount
+            loggedAccounts = oldAccount
+
+            re = json.loads(r.text) #response from server
+
+            uid = re["uid"]
+            nickname = re["nickname"]
+            goldCoin = re["goldCoin"]
+            vip = re["userLevel"]
+
+            reply_text = (
+                "Your account is successfully added!\n\n"
+                f"UID: {uid}\n"
+                f"Nickname: {nickname}\n"
+                f"VIP: {vip}\n"
+                f"Balance: ${roundDown(goldCoin, 4)}"
+            )
+
+            await update.message.reply_text(reply_text, reply_markup=logged_markup) 
+
+        except HTTPError as ex:
+            reply_text = f"{phone} ---> Error: {ex}"
+            if context.user_data.get("account"):
+                await update.message.reply_text(reply_text, reply_markup=logged_markup)
+            else:
+                await update.message.reply_text(reply_text, reply_markup=start_markup)
+
+    return LOGGED_IN
 
 async def add_account(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     reply_text = "Please enter your phone number:"
@@ -149,6 +213,7 @@ async def enter_server(update: Update, context: ContextTypes.DEFAULT_TYPE) -> in
         print(f"Temp Accounts: {tempAccount}")
         accounts.append({"phone": tempAccount["phone"], "pass": tempAccount["pass"], "server": tempAccount["server"]})
         context.user_data["account"] = accounts
+        loggedAccounts = accounts
 
         re = json.loads(r.text) #response from server
         
@@ -222,7 +287,9 @@ async def check_balance(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
 
 
 async def re_login(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global loggedAccounts
     accounts = context.user_data.get("account")
+    loggedAccounts = accounts
     
     for acc in accounts:
         server = acc["server"]
@@ -262,13 +329,67 @@ async def re_login(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def log_out(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    await update.message.reply_text(
+        f"Last accounts json: {facts_to_str(context.user_data)}You use it next time!",
+    )
+
     context.user_data.pop("account")
     reply_text = "You're successfully log out."
 
     await update.message.reply_text(reply_text, reply_markup=start_markup)
     return ADD_ACC
 
-async def competition(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def set_bet_timer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    global timer
+    """Add a job to the queue."""
+    chat_id = update.effective_message.chat_id
+    try:
+        # args[0] should contain the time for the timer in seconds
+        due = float(context.args[0])
+        if due < 0:
+            await update.effective_message.reply_text("Sorry we can not go back to pass!")
+            return
+
+        timer = due
+
+        await showCompetition(update=update, context=context, isTimer=True)
+
+        # job_removed = remove_job_if_exists(str(chat_id), context)
+        # context.job_queue.run_once(alarm, due, chat_id=chat_id, name=str(chat_id), data=due)
+
+        # text = "Timer successfully set!"
+        # if job_removed:
+        #     text += " Old one was removed."
+        # await update.effective_message.reply_text(text)
+
+    except (IndexError, ValueError):
+        await update.effective_message.reply_text("Usage: /set <seconds>")
+
+
+async def unset_bet(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Remove the job if the user changed their mind."""
+    chat_id = update.message.chat_id
+    job_removed = remove_job_if_exists((f"orderTimer{chat_id}"), context)
+    text = "Timer successfully cancelled!" if job_removed else "You have no active timer."
+    await update.message.reply_text(text)
+
+async def alarm(context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send the alarm message."""
+    print(context.user_data)
+    job = context.job
+    await context.bot.send_message(job.chat_id, text=f"Beep! {job.data} seconds are over!")
+
+
+def remove_job_if_exists(name: str, context: ContextTypes.DEFAULT_TYPE) -> bool:
+    """Remove job with given name. Returns whether job was removed."""
+    current_jobs = context.job_queue.get_jobs_by_name(name)
+    if not current_jobs:
+        return False
+    for job in current_jobs:
+        job.schedule_removal()
+    return True
+
+async def showCompetition(update: Update, context: ContextTypes.DEFAULT_TYPE, isTimer: bool = False):
     accounts = context.user_data.get("account")
     if len(accounts) <= 0:
         reply_text = "No Account"
@@ -287,21 +408,23 @@ async def competition(update: Update, context: ContextTypes.DEFAULT_TYPE):
         
         competitions = list(re["competitionInfos"])
 
-        keyboard = []
+        if len(competitions) == 0:
+            await update.message.reply_text("There is no competition!", reply_markup=logged_markup)
+            return
 
         for competition in competitions:
             leagueName = competition["leagueNameEn"]
             homeTeamName = competition["homeTeamNameEn"]
             awayTeamName = competition["awayTeamNameEn"]
             cid = competition["cid"]
-            data = ("info///"
+            option = "timer" if isTimer else "info"
+            data = (f"{option}///"
                 f"{cid}///"
                 f"{server}"
             )
 
-            betButton = [InlineKeyboardButton("Info", callback_data=str(data))]
-            keyboard.append(betButton)
-            reply_markup = InlineKeyboardMarkup(keyboard)
+            betButton = [InlineKeyboardButton("Info", callback_data=data)]
+            reply_markup = InlineKeyboardMarkup([betButton])
 
             reply_text = (
                 f"{leagueName}\n\n"
@@ -314,6 +437,8 @@ async def competition(update: Update, context: ContextTypes.DEFAULT_TYPE):
         reply_text = f"--> ERROR: {ex}"
         await update.message.reply_text(reply_text, reply_markup=logged_markup)
 
+async def competition(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await showCompetition(update=update, context=context)
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Parses the CallbackQuery and updates the message text."""
@@ -328,12 +453,12 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     option = data[0]
     cid = data[1]
     
-    if option == 'info':
+    if option == 'info' or option == 'timer':
         server = data[2]
         url = (f"{server}/api/competition/info")
         params = {
             'cid':cid
-        } 
+        }
 
         try:
             r = requests.get(url, params=params)
@@ -349,12 +474,15 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             quotaList = re['quotaList']
             keyboard = []
 
+            orderOption = "orderTimer" if option == "timer" else "order"
+
             for quota in quotaList:
                 rate = roundDown(quota['rate'] * 100, 2)
                 score = quota['score']
                 scoreStr = str(score).replace("H", "").replace("A", "-")
+                
                 data = (
-                    "order///"
+                    f"{orderOption}///"
                     f"{cid}///"
                     f"{score}"
                 )
@@ -373,7 +501,25 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
         except HTTPError as ex:
             reply_text = f"--> ERROR: {ex}"
+            print(reply_text)
             await query.edit_message_text(reply_text, reply_markup=logged_markup)
+
+        return
+
+    if option == 'orderTimer':
+        odds = data[2]
+        chatID = update.effective_message.chat_id
+        data = f"{cid}///{odds}"
+
+        job_removed = remove_job_if_exists((f"orderTimer{chatID}"), context)
+        context.job_queue.run_once(orderTimer, timer, chat_id=chatID, name=(f"orderTimer{chatID}"), data=data)
+
+        text = f"Timer bet successfully set {timer}!"
+        if job_removed:
+            text += " Old one was removed."
+    
+        await query.edit_message_text(text)
+        return
 
     if option == 'order':
         odds = data[2]
@@ -384,6 +530,9 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             await query.edit_message_text("No account!", reply_markup=logged_markup)
         else:
             accounts = context.user_data.get('account')
+
+        text = f"Betting is in progress..."
+        await query.edit_message_text(text=text)
 
         for account in accounts:
             server = account['server']
@@ -415,7 +564,7 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
                     if re['code'] != 200:
                         msg = re['msg']
-                        reply_text = f"{phone} --> ERROR: {msg}"
+                        reply_text = f"{phone} --> {msg}"
                         await context.bot.send_message(context._user_id, text=reply_text, reply_markup=logged_markup)
                     
                 except HTTPError as ex:
@@ -428,8 +577,70 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
         text = f"All {len(accounts)} accounts has been betted at {score}"
         await query.edit_message_text(text=text)
+        return
 
-    # await query.edit_message_text(text=f"Selected option: {query.data}")
+async def orderTimer(context: ContextTypes.DEFAULT_TYPE):
+    print("Order timer")
+    job = context.job
+    data = str(job.data).split("///")
+    cid = data[0]
+    odds = data[1]
+    score = str(odds).replace("H", "").replace("A", "-")
+
+    if len(loggedAccounts) == 0:
+        text = "No account"
+        await context.bot.send_message(job.chat_id, text=text, reply_markup=logged_markup)
+    else:
+        accounts = loggedAccounts
+
+        text = f"Timer Bet is in progress..."
+        await context.bot.send_message(job.chat_id, text=text, reply_markup=logged_markup)
+
+        for account in accounts:
+            server = account['server']
+            phone = account['phone']
+            url = (f"{server}/api/info")
+            print(f"URL: {url}")
+
+            try:
+                r = requests.post(url, json={})
+                r.raise_for_status()
+
+                re = json.loads(r.text) #response from server
+
+                goldCoin = re["goldCoin"]
+                url = f"{server}/api/competition/order"
+                payload = {
+                    'cid':cid,
+                    "amount": roundDown(goldCoin, 4),
+                    "odds": odds
+                } 
+
+                try:
+                    r = requests.post(url, json=payload)
+                    r.raise_for_status()
+
+                    re = json.loads(r.text) #response from server
+                    # print(r.status_code)
+                    # print(re)
+
+                    if re['code'] != 200:
+                        msg = re['msg']
+                        reply_text = f"{phone} --> {msg}"
+                        await context.bot.send_message(job.chat_id, text=reply_text, reply_markup=logged_markup)
+                    
+                except HTTPError as ex:
+                    reply_text = f"{phone} --> ERROR: {ex}"
+                    await context.bot.send_message(job.chat_id, text=reply_text, reply_markup=logged_markup)
+
+            except HTTPError as ex:
+                reply_text = f"{phone} --> ERROR: {ex}"
+                await context.bot.send_message(job.chat_id, text=reply_text, reply_markup=logged_markup)
+
+        text = f"All {len(accounts)} accounts has been betted at {score}"
+        await context.bot.send_message(job.chat_id, text=text, reply_markup=logged_markup)
+    
+    return
 
 async def record(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     accounts = context.user_data.get("account")
@@ -497,9 +708,12 @@ async def done(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
         del context.user_data["choice"]
 
     await update.message.reply_text(
-        f"I learned these facts about you: {facts_to_str(context.user_data)}Until next time!",
+        f"Last accounts json: {facts_to_str(context.user_data)}You use it next time!",
         reply_markup=ReplyKeyboardRemove(),
     )
+
+    context.user_data.pop("account")
+
     return ConversationHandler.END
 
 
@@ -514,7 +728,16 @@ def main() -> None:
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
+            ADD_MULTI: [
+                MessageHandler(
+                    filters.TEXT & ~filters.COMMAND, json_to_accounts
+                )
+            ],
             ADD_ACC: [
+                MessageHandler(
+                    # filters.Regex("^(Age|Favourite colour|Number of siblings)$"), regular_choice
+                    filters.Regex("^(Add Multi)$"), add_multi
+                ),
                 MessageHandler(
                     # filters.Regex("^(Age|Favourite colour|Number of siblings)$"), regular_choice
                     filters.Regex("^(Add New Account)$"), add_account
@@ -549,10 +772,19 @@ def main() -> None:
                     filters.Regex("^Bet record$"), record
                 ),
                 MessageHandler(
-                    filters.Regex("^Re-Login$"), re_login
+                    filters.Regex("^Login$"), re_login
+                ),
+                CommandHandler(
+                    "SetBet", set_bet_timer
+                ),
+                CommandHandler(
+                    "UnSetBet", unset_bet
                 ),
                 MessageHandler(
                     filters.Regex("^Add New Account$"), add_account
+                ),
+                MessageHandler(
+                    filters.Regex("^Add Multi$"), add_multi
                 )
             ],
         },
